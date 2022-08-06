936b2f66d7ff909d70ba99715fe64b3d10a39dc3
Prateek <prateekdubey19999@gmail.com>
2022-08-06 16:18:39 +0530

    second

---
+++ phase2 program/MainProject/ValidationOfUserLogin/src/main/java/ValidationOfUserLogin/Logout.java
@@ -0,0 +1,15 @@
+package ValidationOfUserLogin;
+import java.io.PrintWriter;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Logout extends HttpServlet{
+	public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException {
+		 PrintWriter out = response.getWriter();      
+		out.print("ThankYou For Visiting");
+	}
+	}
+
